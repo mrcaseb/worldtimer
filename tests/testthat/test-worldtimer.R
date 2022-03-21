e60f368fc669552d8c3bde5086f0ae358684da35
mrcaseb <roemerle@gmail.com>
2022-03-21 22:17:11 +0100

    test lubridate parser

--- tests/testthat/test-worldtimer.R
+++ tests/testthat/test-worldtimer.R
@@ -20,3 +20,15 @@ test_that("timezones are returned", {
   expect_type(tzs, "list")
 })
 
+test_that("lubridate parser works", {
+  iso_time_string <- "2022-03-21T17:10:21.061284-04:00"
+  parsed <- lubridate::parse_date_time2(
+    iso_time_string, orders = "YmdHMOSz"
+  )
+  expect_true(inherits(parsed, c("POSIXct", "POSIXt")))
+  expect_true(lubridate::is.POSIXct(parsed))
+  expect_true(lubridate::is.POSIXt(parsed))
+  expect_false(lubridate::is.POSIXlt(parsed))
+  expect_type(parsed, "double")
+})
+
